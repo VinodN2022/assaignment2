13a6028732397049ec46a348c1622c860d51022a
vinod <nvinodnvn1000@gmail.com>
2022-03-10 00:12:30 +0530

    maven3

---
+++ LearnSelenium/src/seleniumprograms/VerifyRegistration.java
@@ -0,0 +1,18 @@
+package seleniumprograms;
+
+
+
+public class VerifyRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium_tool\\driver\\chromedriver_win32\\chromedriver.exe");
+	 
+			
+			
+		
+		
+		
+	}
+
+}
